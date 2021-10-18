4b43a93e6e7a50376b696f5881055002fec47f9a
Juanma L <juanmalubian@gmail.com>
2021-10-18 10:25:35 +0200

    Clase Singleton añadida

--- src/test/java/com/bootcamp/PracticaMicroservicios/PracticaMicroserviciosApplicationTests.java
+++ src/test/java/com/bootcamp/PracticaMicroservicios/PracticaMicroserviciosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bototcamp.PracticaMicroservicios;
+package com.bootcamp.PracticaMicroservicios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
